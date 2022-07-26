5cc44cbb2e2b7eb534ebdc2bca2cb825449f185d
Motoki Kimura <motoki.kimura.1990@gmail.com>
2022-07-26 03:45:45 +0000

    add meke_folds_v3.py

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -2,6 +2,7 @@
 
 python tools/make_folds_v1.py
 python tools/make_folds_v2.py
+python tools/make_folds_v3.py
 python tools/make_test_csv.py
 python tools/prepare_building_masks.py
 python tools/prepare_road_masks.py
