6127280a2b534874e383a69b4bc0cb77386cb32c
Motoki Kimura <motoki.kimura.1990@gmail.com>
2022-07-31 11:45:11 +0000

    fix

--- scripts/val_all_folds.sh
+++ scripts/val_all_folds.sh
@@ -8,7 +8,7 @@ echo EXP_ID: $EXP_ID
 echo TEST_ARGS: $TEST_ARGS
 
 # XXX: 5-fold
-#python tools/test_net.py --val --exp_id $(($EXP_ID+0)) $TEST_ARGS
+python tools/test_net.py --val --exp_id $(($EXP_ID+0)) $TEST_ARGS
 python tools/test_net.py --val --exp_id $(($EXP_ID+1)) $TEST_ARGS
 python tools/test_net.py --val --exp_id $(($EXP_ID+2)) $TEST_ARGS
 python tools/test_net.py --val --exp_id $(($EXP_ID+3)) $TEST_ARGS
