50c0dc2ec653b6f828ea84037c9c8cd0cfb098b7
Motoki Kimura <motoki.kimura.1990@gmail.com>
2022-07-31 11:44:54 +0000

    add val scripts

---
+++ scripts/val_road.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# example usage: ./scripts/val_road.sh 10100 15100
+
+foundation=$1
+flood=$2
+
+python tools/road/vectorize.py --val --foundation /wdata/_val/preds/exp_$foundation
+python tools/road/to_graph.py --val --vector /wdata/_val/road_vectors/exp_$foundation
+python tools/road/insert_flood.py --val --graph /wdata/_val/road_graphs/exp_$foundation --flood /wdata/_val/preds/exp_$flood
