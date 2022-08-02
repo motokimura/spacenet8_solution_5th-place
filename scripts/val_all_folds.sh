d14fad82b93bb1f8b35757ef088aa22ecffa7c88
Motoki Kimura <motoki.kimura.1990@gmail.com>
2022-08-02 07:46:29 +0000

    minor fix

--- scripts/val_all_folds.sh
+++ scripts/val_all_folds.sh
@@ -4,12 +4,4 @@
 EXP_ID=$1
 TEST_ARGS=${@:2}
 
-echo EXP_ID: $EXP_ID
-echo TEST_ARGS: $TEST_ARGS
-
-# XXX: 5-fold
-python tools/test_net.py --val --exp_id $(($EXP_ID+0)) $TEST_ARGS
-python tools/test_net.py --val --exp_id $(($EXP_ID+1)) $TEST_ARGS
-python tools/test_net.py --val --exp_id $(($EXP_ID+2)) $TEST_ARGS
-python tools/test_net.py --val --exp_id $(($EXP_ID+3)) $TEST_ARGS
-python tools/test_net.py --val --exp_id $(($EXP_ID+4)) $TEST_ARGS
+./scripts/test_all_folds.sh $EXP_ID --val $TEST_ARGS
