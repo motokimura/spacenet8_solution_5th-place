db6f8a2ea0097fdcbffcd8f924bfec4e7d19e055
Motoki Kimura <motoki.kimura.1990@gmail.com>
2022-08-18 09:02:13 +0000

    update train.sh and test.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+START_TIME=$SECONDS
+
 TRAIN_DIR=$1
+
+# remove motokimura"s home-built models
+rm -rf /work/models
 
 # preprocess
 python tools/make_folds_v3.py --train_dir $TRAIN_DIR
@@ -8,8 +13,8 @@ python tools/prepare_building_masks.py --train_dir $TRAIN_DIR
 python tools/prepare_road_masks.py --train_dir $TRAIN_DIR
 python tools/warp_post_images.py --root_dir $TRAIN_DIR
 
-echo 'mosaicing.. this will take ~20 mins'
-echo 'you can check progress from /wdata/mosaics/train_*.txt'
+echo "mosaicing.. this will take ~20 mins"
+echo "you can check progress from /wdata/mosaics/train_*.txt"
 mkdir -p /wdata/mosaics/
 nohup python tools/mosaic.py --fold_id 0 > /wdata/mosaics/train_0.txt --train_dir $TRAIN_DIR 2>&1 &
 nohup python tools/mosaic.py --fold_id 1 > /wdata/mosaics/train_1.txt --train_dir $TRAIN_DIR 2>&1 &
@@ -22,3 +27,6 @@ python tools/measure_image_similarities.py --train_dir $TRAIN_DIR --train_only
 
 # training
 # TODO
+
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Total time for training: " $(($ELAPSED_TIME / 60 + 1)) "[min]"
